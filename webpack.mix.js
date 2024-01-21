163a516b2ee667ac1e61b5d6b26dfaff9efd0ab9
Emanuel S. Pedrozo <mr.espedrozo@gmail.com>
2024-01-21 18:04:39 -0300

    updating deploy

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,10 @@
 const mix = require('laravel-mix');
 
-mix.js('public/js/frontConnection.js', 'public/js')
-    //.js('public/js/frontConnection.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [        
-]);
+mix.js('public/js/app.js', 'public/js')
+   .postCss('resources/css/app.css', 'public/css');
 
 mix.webpackConfig({
-    resolve: {
-        extensions: ['.js', '.json']
-    }
+   resolve: {
+      extensions: ['.js', '.json']
+   }
 });
